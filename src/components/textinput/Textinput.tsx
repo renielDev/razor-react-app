4d4f41f25153ec205ac332e0ce37d1d7f10a36eb
renyel <reniel.salvador@gmail.com>
2023-10-02 03:28:52 +0800

    chore: resolve lint issue

--- src/components/textinput/Textinput.tsx
+++ src/components/textinput/Textinput.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { cn } from '../../shared/utils';
 
 interface TextInputProps extends React.InputHTMLAttributes<HTMLInputElement> {
-};
+}
 
 const TextInput: React.FC<TextInputProps> = ({ className, ...props }) => {
   const _className = cn('px-2 py-1 text-sm border-gray-200 rounded border-1', className)
